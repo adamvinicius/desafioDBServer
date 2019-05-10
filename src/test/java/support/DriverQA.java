74efcae13ce4b9980de0b6403d3645b84b8a2f8f
adamvinicius <adamvinicius@gmail.com>
2019-05-10 20:26:02 -0300

    finalizacao desafio DBServer

--- src/test/java/support/DriverQA.java
+++ src/test/java/support/DriverQA.java
@@ -1,6 +1,5 @@
 package support;
 
-
 import java.io.File;
 import java.io.IOException;
 
@@ -12,63 +11,83 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 public class DriverQA {
-	
+
 	private static ChromeOptions options;
-	
+	private static FirefoxOptions optionsFirefox;
+
 	private static WebDriver driver;
-	
-    public static void start(String endereco){
-    	options = new ChromeOptions();
-    	options.addArguments("--headless");
+
+	public static void startChrome(String endereco) {
+		options = new ChromeOptions();
+		options.addArguments("--headless");
 		options.addArguments("window-size=1200x800");
-    	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-    	driver = new ChromeDriver(options);
-    	
-    	driver.get(endereco);
-    	driver.manage().window().maximize();
-   }
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver(options);
+
+		driver.get(endereco);
+		driver.manage().window().maximize();
+	}
+
+	public static void startFirefox(String endereco) {
+		optionsFirefox = new FirefoxOptions();
+		optionsFirefox.addArguments("--headless");
+		optionsFirefox.addArguments("window-size=1200x800");
+		System.setProperty("webdriver.chrome.driver", "geckodriver.exe");
+		driver = new FirefoxDriver(optionsFirefox);
+
+		driver.get(endereco);
+		driver.manage().window().maximize();
+	}
+	
+	public static void start(String endereco) {
+		try {
+			startChrome(endereco);
+		} catch (Exception e) {
+			startFirefox(endereco);
+		}
+	}
 
 	public static WebElement findElement(String campo, String tipo) {
-		
+
 		WebElement element;
-		
+
 		if (tipo.equals("xpath")) {
 			element = driver.findElement(By.xpath(campo));
-		}else if (tipo.equals("id")) {
+		} else if (tipo.equals("id")) {
 			element = driver.findElement(By.id(campo));
-		}else if (tipo.equals("css")) {
+		} else if (tipo.equals("css")) {
 			element = driver.findElement(By.cssSelector(campo));
-		}else {
+		} else {
 			element = null;
 		}
-		
+
 		return element;
-		
+
 	}
-	
+
 	public static void click(String campo, String tipo) {
-		
+
 		findElement(campo, tipo).click();
 	}
-	
+
 	public static void waitElement(String campo, String tipo) {
 		WebDriverWait driverWait = new WebDriverWait(driver, 15);
-		
+
 		if (tipo.equals("xpath")) {
 			driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(campo)));
-		}else if (tipo.equals("id")) {
+		} else if (tipo.equals("id")) {
 			driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id(campo)));
-		}else if (tipo.equals("css")) {
+		} else if (tipo.equals("css")) {
 			driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(campo)));
 		}
-		
-		
+
 	}
 
 	public static String getText(String campo, String tipo) {
@@ -78,32 +97,28 @@ public class DriverQA {
 	public static void sendKeys(String texto, String campo, String tipo) {
 		findElement(campo, tipo).clear();
 		findElement(campo, tipo).sendKeys(texto);
-		
+
 	}
-	
+
 	public static void selectText(String texto, String campo, String tipo) {
 		WebElement element = findElement(campo, tipo);
-        Select dropdown = new Select(element);
-        try {
+		Select dropdown = new Select(element);
+		try {
 			Thread.currentThread();
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-        
-        dropdown.selectByVisibleText(texto);
+
+		dropdown.selectByVisibleText(texto);
 	}
-	
+
 	public static void PrintScreen(String nome_arquivo) throws IOException {
-		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		// Now you can do whatever you need to do with it, for example copy somewhere
-		FileUtils.copyFile(scrFile, new File("target/reports/"+nome_arquivo+".png"));
-		
+		FileUtils.copyFile(scrFile, new File("target/reports/" + nome_arquivo + ".png"));
+
 	}
-	
-	
-	
-	
+
 }
